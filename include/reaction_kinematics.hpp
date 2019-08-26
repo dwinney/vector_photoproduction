93de2fcda60436437c9fcce141bbca4478b3aee2
dwinney <dwinney@iu.edu>
2019-08-26 14:36:48 -0400

    added reaction_kinematics class to hold all relevant kinematic quantities and functions for the photoproduction reaction, only depends on the mass of the vector particle being produced

---
+++ include/reaction_kinematics.hpp
@@ -0,0 +1,48 @@
+// Class to contain all relevant kinematic quantities. The kinematics of the reaction
+// gamma p -> V p' is entirely determined by specifying the mass of the vector particle
+//
+// Author:       Daniel Winney (2019)
+// Affiliation:  Joint Physics Analysis Center (JPAC)
+// Email:        dwinney@iu.edu
+// ---------------------------------------------------------------------------
+
+#include "constants.hpp"
+#include "two_body_state.hpp"
+#include "polarization_vector.hpp"
+
+#include <string>
+
+#ifndef _KINEMATICS_
+#define _KINEMATICS_
+
+using std::string;
+
+template <typename T>
+T Kallen(T x, T y, T z)
+{
+  return x*x + y*y + z*z - 2. * (x*y + x*z + y*z);
+};
+
+class reaction_kinematics
+{
+private:
+  // vector meson mass and identifier
+  double mVec;
+  string vector_particle;
+
+  // inital and final state kinematics
+  two_body_state initial, final;
+  polarization_vector eps_vec_star, eps_gamma;
+
+public:
+  reaction_kinematics(double vec_mass, string vec_name)
+  : mVec(vec_mass), vector_particle(vec_name),
+    initial(0., mPro, "beam", "target"),
+    final(vec_mass, mPro, vec_name, "recoil"),
+    eps_vec_star(final, vec_name, true),
+    eps_gamma(initial, "beam")
+  {};
+
+};
+
+#endif
