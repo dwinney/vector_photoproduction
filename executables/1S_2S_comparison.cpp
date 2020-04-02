6201d9ba5d7e05f3c2368b99b5a3d96b1e0d6fb0
dwinney <dwinney@iu.edu>
2020-04-02 15:36:18 -0400

    new executable for the psi2s stuff

---
+++ executables/1S_2S_comparison.cpp
@@ -0,0 +1,110 @@
+// ---------------------------------------------------------------------------
+// Comparing the photoproduction cross-sections of the Jpsi 1s and 2s states
+// near threshold.
+//
+// Author:       Daniel Winney (2020)
+// Affiliation:  Joint Physics Analysis Center (JPAC)
+// Email:        dwinney@iu.edu
+// ---------------------------------------------------------------------------
+// References:
+// [1] 10.1103/PhysRevD.100.034019
+// ---------------------------------------------------------------------------
+
+#include "constants.hpp"
+#include "reaction_kinematics.hpp"
+#include "amplitudes/pomeron_exchange.hpp"
+#include "regge_trajectory.hpp"
+#include "utilities.hpp"
+
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+int main( int argc, char** argv )
+{
+  double theta = 0.;
+  for (int i = 0; i < argc; i++)
+  {
+    if (std::strcmp(argv[i],"-c")==0) theta = atof(argv[i+1]);
+  }
+
+  // Set up kinematics, determined entirely by vector meson mass
+  reaction_kinematics * ptr1s = new reaction_kinematics(3.097, "PSI(1s)");
+  reaction_kinematics * ptr2s = new reaction_kinematics(3.686, "PSI(2s)");
+
+  // Set up pomeron trajectory
+  // Here we use (real) linear trajectory with intercept and slope only free params
+  // Best fit values from [1]
+  linear_traj alpha(0.941, 0.364);
+
+  // Create amplitudes with kinematics and same trajectory
+  pomeron_exchange pomeron_1s(ptr1s, &alpha);
+  pomeron_exchange pomeron_2s(ptr2s, &alpha);
+
+  // Feed in other two parameters (normalization and t-slope)
+
+  // Best fit from [1]
+  std::vector<double> params_1s = {0.379, 0.12};
+  pomeron_1s.set_params(params_1s);
+
+  // Same t-slope but coupling is scaled by 1/4
+  std::vector<double> params_2s = {0.379 / 4., 0.12};
+  pomeron_2s.set_params(params_2s);
+
+  // ---------------------------------------------------------------------------
+  // You shouldnt need to change anything below this line
+  // ---------------------------------------------------------------------------
+
+  // ---------------------------------------------------------------------------
+  // Print .dat files of differential cross section and plot it to a .pdf
+
+  std::vector<amplitude*> amp = {&pomeron_1s, &pomeron_2s};
+  double zs = cos(theta * deg2rad);
+
+  for (int n = 0; n < amp.size(); n++)
+  {
+    std::cout << "\n";
+    std::cout << "Printing DXS for " << amp[n]->kinematics->vector_particle;
+    std::cout << " production at " << theta << " degrees in center-of-mass frame.";
+    std::cout << "\n";
+
+    std::vector<double> s_n, dxs_n;
+    for (int i = 0; i < 100; i++)
+    {
+      double si = (amp[n]->kinematics->sth + 0.5) + double(i) * (900. - (amp[n]->kinematics->sth + 0.5)) / 100.;
+      double dxsi = amp[n]->diff_xsection(si, zs);
+
+      s_n.push_back(sqrt(si));
+      dxs_n.push_back(dxsi);
+    }
+
+    quick_print(s_n, dxs_n, amp[n]->kinematics->vector_particle + "_photoproduction");
+    quick_plot(s_n, dxs_n, amp[n]->kinematics->vector_particle + "_photoproduction");
+  }
+  std::cout << "\n";
+
+  // ---------------------------------------------------------------------------
+  // Do the same for the ratio though
+  std::cout << "Printing ratio of cross-sections. \n";
+
+  std::vector<double> s, ratio;
+  for (int i = 0; i < 100; i++)
+  {
+    double si = (ptr2s->sth + 0.5) + double(i) * (900. - (ptr2s->sth + 0.5)) / 100.;
+    double ratioi = pomeron_2s.diff_xsection(si, zs) / pomeron_1s.diff_xsection(si, zs);
+
+    s.push_back(sqrt(si));
+    ratio.push_back(ratioi);
+  }
+
+  quick_print(s, ratio, "dxs_ratio");
+  quick_plot(s, ratio, "dxs_ratio");
+  std::cout << "\n";
+
+  delete ptr1s, ptr2s;
+
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+  return 1.;
+};
