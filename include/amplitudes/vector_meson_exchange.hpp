a0467cd8e939bf4a529a0f59ee315ebc6d949a4e
dwinney <dwinney@iu.edu>
2020-04-06 13:29:12 -0400

    changed metric[nu] to two index metric[mu][nu]

--- include/amplitudes/vector_meson_exchange.hpp
+++ include/amplitudes/vector_meson_exchange.hpp
@@ -8,14 +8,18 @@
 #ifndef _AXIAL_
 #define _AXIAL_
 
+#include <string>
+#include <vector>
+#include <iostream>
+
 #include "amplitude.hpp"
 
-class vector_meson_exchange : public helicity_amplitude
+class vector_meson_exchange : public amplitude
 {
 public:
   // Constructor
   vector_meson_exchange(reaction_kinematics * xkinem, double mass)
-  : amplitude(xkinem). mEx2(mass*mass)
+  : amplitude(xkinem), mEx2(mass*mass)
   {};
 
   // Copy constructor
@@ -34,22 +38,28 @@ public:
 
   // Assemble the helicity amplitude by contracting the lorentz indices
   std::complex<double> helicity_amplitude(std::vector<int> helicities, double s, double zs);
-  
+
 private:
   // Mass of the exchange
   double mEx2;
 
   // Couplings to the axial-vector/photon and vector/tensor couplings to nucleon
-  double gGamma, gV, gT;
+  double gGamma = 0., gV = 0., gT = 0.;
+
+  // Four dimensional Levi-Civita symbol
+  double LeviCivita(int mu, int alpha, int beta, int gamma);
+
+  // Gamma matric tensor
+  std::complex<double> Sigma(int mu, int nu, int i, int j);
 
   // Four-momentum of the exhange
-  std::complex<double> vector_momenta(int mu, double s, double zs);
+  std::complex<double> exchange_momenta(int mu, double s, double zs);
 
   // Photon - Axial Vector - Vector vertex
-  std::complex<double> top_vertex(int mu, int nu, int lam_gam, int lam_vec, double s, double zs);
+  std::complex<double> top_vertex(int mu, int lam_gam, int lam_vec, double s, double zs);
 
   // Nucleon - Nucleon - Vector vertex
-  std::complex<double> bottom_vertex(int mu, int nu, int lam_targ, int lam_rec, double s, double zs);
+  std::complex<double> bottom_vertex(int nu, int lam_targ, int lam_rec, double s, double zs);
 
   // Vector propogator
   std::complex<double> vector_propagator(int mu, int nu, double s, double zs);
