76baf9bfb48149d286b3bdc05a4eb66788fefc59
dwinney <dwinney@iu.edu>
2020-06-21 13:35:18 -0400

    final version of Zc exe

--- executables/Zc_photoproduction.cpp
+++ executables/Zc_photoproduction.cpp
@@ -16,6 +16,7 @@
 #include "amplitudes/pseudoscalar_exchange.hpp"
 
 #include "jpacGraph1D.hpp"
+#include "jpacUtils.hpp"
 
 #include <cstring>
 
@@ -23,87 +24,124 @@ using namespace jpacPhoto;
 
 int main( int argc, char** argv )
 {
-  double theta = 0.;
+
+  // ---------------------------------------------------------------------------
+  // COMMAND LINE OPTIONS
+  // ---------------------------------------------------------------------------
+
+  double zs = 1.;
   double max = 25;
-  double y[2] = {0., 0.15};
+  double y[2]; bool custom_y = false;
   int N = 50;
-  std::string filename = "pseudoscalar_exchange.pdf";
-  bool integ = true;
+  std::string ylabel = "#sigma  (nb)";
+  std::string filename = "Zc_photoproduction.pdf";
+  bool INTEG = true;
+
+  // ---------------------------------------------------------------------------
+  // Parse command line arguments
   for (int i = 0; i < argc; i++)
   {
     if (std::strcmp(argv[i],"-f")==0) filename = argv[i+1];
-    if (std::strcmp(argv[i],"-c")==0) theta = atof(argv[i+1]);
+    if (std::strcmp(argv[i],"-c")==0) zs = cos(atof(argv[i+1]) * deg2rad);
     if (std::strcmp(argv[i],"-m")==0) max = atof(argv[i+1]);
     if (std::strcmp(argv[i],"-n")==0) N = atoi(argv[i+1]);
-    if (std::strcmp(argv[i],"-y")==0) y_range(argv[i+1], y);
-    if (std::strcmp(argv[i],"-diff")==0) integ = false;
+    if (std::strcmp(argv[i],"-y")==0)
+    {
+      custom_y = true;
+      y_range(argv[i+1], y);
+    }
+    if (std::strcmp(argv[i],"-diff")==0)
+    {
+      INTEG = false;
+      ylabel = "d#sigma/dt  (#mub GeV^{-2})";
+    }
   }
 
-  // Set up kinematics for the chi_c1
-  reaction_kinematics * ptr = new reaction_kinematics(4.20, "Z_{c}^{+}(4200)");
+  // ---------------------------------------------------------------------------
+  // AMPLITUDES
+  // ---------------------------------------------------------------------------
 
-  // Simple pi exchange
-  pseudoscalar_exchange amp0(ptr, mPi, "#pi meson exchange");
-
-  // Compare with the trajectory exchange
+  // For regge amps need pion trajectory
   linear_trajectory alpha(1, -0.7*mPi*mPi, 0.7, "pionic trajectory");
-  pseudoscalar_exchange amp1(ptr, &alpha, "Pionic Regge Trajectory");
 
-  // Same couplings
-  amp0.set_params({1.731 * 4.20, sqrt(4.*M_PI*14.4)});
-  amp1.set_params({1.731 * 4.20, sqrt(4.*M_PI*14.4)});
+  // ---------------------------------------------------------------------------
+  // ZC(3900)
+
+  // Kinematics for 3900 MeV vector
+  reaction_kinematics * ptr3900 = new reaction_kinematics(3.9, "Z_{c}^{+}(3900)");
+
+  // Amplitudes
+  pseudoscalar_exchange Z3900(ptr3900, mPi, "Z_{c}^{+}(3900), #pi exchange");
+  pseudoscalar_exchange Z3900R(ptr3900, &alpha, "Z_{c}^{+}(3900), Reggeon exchange");
+
+  // Couplings for 4 MeV width
+  Z3900.set_params({0.67 * 3.90, sqrt(4.*M_PI*14.4)});
+  Z3900R.set_params({0.67 * 3.90, sqrt(4.*M_PI*14.4)});
+
+
+  // ---------------------------------------------------------------------------
+  // ZC(4200)
+
+  // Kinematics for 4200
+  reaction_kinematics * ptr4200 = new reaction_kinematics(4.20, "Z_{c}^{+}(4200)");
+
+  // Amplitudes
+  pseudoscalar_exchange Z4200(ptr4200, mPi, "Z_{c}^{+}(4200), #pi exchange");
+  pseudoscalar_exchange Z4200R(ptr4200, &alpha, "Z_{c}^{+}(4200), Reggeon exchange");
+
+  // Couplings
+  Z4200.set_params({1.731 * 4.20, sqrt(4.*M_PI*14.4)});
+  Z4200R.set_params({1.731 * 4.20, sqrt(4.*M_PI*14.4)});
+  // ---------------------------------------------------------------------------
 
   // Add to a vector to plot them both
   std::vector<amplitude*> amps;
-  amps.push_back(&amp0);
-  amps.push_back(&amp1);
+  amps.push_back(&Z4200);
+  amps.push_back(&Z4200R);
+  amps.push_back(&Z3900);
+  amps.push_back(&Z3900R);
 
   // ---------------------------------------------------------------------------
   // You shouldnt need to change anything below this line
   // ---------------------------------------------------------------------------
-  double zs = cos(theta * deg2rad);
+
+  // Plotter object
   jpacGraph1D* plotter = new jpacGraph1D();
 
+  // ---------------------------------------------------------------------------
+  // Print the desired observable for each amplitude
   for (int n = 0; n < amps.size(); n++)
   {
-    std::cout << "\nPrinting amplitde: " << amps[n]->identifier << ". \n";
-    std::vector<double> W, dxs;
-    for (int i = 0; i <= N; i++)
+    std::cout << std::endl << "Printing amplitude: " << amps[n]->identifier << "\n";
+    auto F = [&](double W)
     {
-      double Wi = (sqrt(ptr->sth) + EPS) + double(i) * (max - (sqrt(ptr->sth) + EPS)) / N;
-      W.push_back(Wi);
-
-      double xsi, si = Wi*Wi;
-      if (integ == false)
+      if (INTEG == false)
       {
-        xsi = amps[n]->differential_xsection(si, zs) * 1.e-3;
+        return amps[n]->differential_xsection(W*W, zs);
       }
       else
       {
-        xsi = amps[n]->integrated_xsection(si) * 1.e-3;
+        return amps[n]->integrated_xsection(W*W);
       }
+    };
 
-      dxs.push_back(xsi);
 
-      debug(i, si, xsi);
-    }
-
-    plotter->AddEntry(W, dxs, amps[n]->identifier);
+    std::array<std::vector<double>, 2> x_fx = vec_fill(N, F, sqrt(amps[n]->kinematics->sth), max, true);
+    plotter->AddEntry(x_fx[0], x_fx[1], amps[n]->identifier);
   }
 
-  std::string ylabel;
-  if (integ == false)
-  {
-    ylabel = "d#sigma/dt  (#mub GeV^{-2})";
-  }
-  else
-  {
-    ylabel = "#sigma  (#mub)";
-  }
+  // Set up X-axis
+  plotter->SetXaxis("W  (GeV)", sqrt(ptr3900->sth), max);
 
-  plotter->SetYaxis(ylabel, y[0], y[1]);
-  plotter->SetXaxis("W  (GeV)", sqrt(ptr->sth), max);
-  plotter->SetLegend(0.55, 0.55);
+  // To change the range of the Y-axis or the position of the Legend change the arguments here
+  (custom_y == true) ? (plotter->SetYaxis(ylabel, y[0], y[1])) : (plotter->SetYaxis(ylabel));
 
+  // Position of the legend
+  plotter->SetLegend(0.5, 0.65);
+
+  // Output to file
   plotter->Plot(filename);
+
+  delete ptr4200, ptr3900, plotter;
+  return 1.;
 }
