508fb75bea6c26078d4f3603a4ddc1f5eb63f087
Daniel Winney <dwinney@iu.edu>
2020-12-02 00:45:46 -0500

    cleaned everything up. vector kinematics only

--- src/amplitudes/baryon_resonance.cpp
+++ src/amplitudes/baryon_resonance.cpp
@@ -10,69 +10,72 @@
 // Combined amplitude as a Breit-Wigner with the residue as the prodect of hadronic and photo-couplings
 std::complex<double> jpacPhoto::baryon_resonance::helicity_amplitude(std::array<int, 4> helicities, double xs, double xt)
 {
-  int lam_i = 2 * helicities[0] - helicities[1];
-  int lam_f = 2 * helicities[2] - helicities[3];
+    int lam_i = 2 * helicities[0] - helicities[1];
+    int lam_f = 2 * helicities[2] - helicities[3];
 
-  // update save values of energies and angle
-  s = xs; t = xt; theta = kinematics->theta_s(xs, xt);
+    // update save values of energies and angle
+    s = xs; t = xt; theta = kinematics->theta_s(xs, xt);
 
-  std::complex<double> residue = 1.;
-  residue  = photo_coupling(lam_i);
-  residue *= hadronic_coupling(lam_f);
-  residue *= threshold_factor(1.5);
+    std::complex<double> residue = 1.;
+    residue  = photo_coupling(lam_i);
+    residue *= hadronic_coupling(lam_f);
+    residue *= threshold_factor(1.5);
 
-  residue *= wigner_d_half(J, lam_i, lam_f, theta);
-  residue /= (s + xi * mRes * gamRes - mRes*mRes);
+    residue *= wigner_d_half(J, lam_i, lam_f, theta);
+    residue /= (s + xi * mRes * gamRes - mRes*mRes);
 
-  return residue;
+    return residue;
 };
 
 // Ad-hoc threshold factor to kill the resonance at threshold
 double jpacPhoto::baryon_resonance::threshold_factor(double beta)
 {
-  double result = pow((s - kinematics->sth()) / s, beta);
-  result /= pow((mRes*mRes - kinematics->sth()) / (mRes*mRes), beta);
+    double result = pow((s - kinematics->sth()) / s, beta);
+    result /= pow((mRes*mRes - kinematics->sth()) / (mRes*mRes), beta);
 
-  return result;
+    return result;
 };
 
 // Photoexcitation helicity amplitude for the process gamma p -> R
 std::complex<double> jpacPhoto::baryon_resonance::photo_coupling(int lam_i)
 {
-  // A_1/2 or A_3/2 depending on ratio R_photo
-  double a;
-  (std::abs(lam_i) == 1) ? (a = R_photo) : (a = sqrt(1. - R_photo * R_photo));
+    // For spin-1/2 exchange no double flip
+    if (J == 1 && abs(lam_i) > 1) return 0.;
 
-  // Electromagnetic decay width given by VMD assumption
-  std::complex<double> emGamma = (xBR * gamRes) * pow(fJpsi / mJpsi, 2.);
-  emGamma *= pow(xr * pi_bar / pf_bar, double(2 * l_min + 1)) * P_t;
+    // A_1/2 or A_3/2 depending on ratio R_photo
+    double a;
+    (std::abs(lam_i) == 1) ? (a = R_photo) : (a = sqrt(1. - R_photo * R_photo));
 
-  // Photo-coupling overall size of |A_1/2|^2 + |A_3/2|^2 is restriced from VMD
-  std::complex<double> A_lam = emGamma * M_PI * mRes * double(J + 1) / (2. * mPro * pi_bar * pi_bar);
-  A_lam = sqrt(xr * A_lam);
+    // Electromagnetic decay width given by VMD assumption
+    std::complex<double> emGamma = (xBR * gamRes) * pow(fJpsi / mJpsi, 2.);
+    emGamma *= pow(xr * pi_bar / pf_bar, double(2 * l_min + 1)) * P_t;
 
-  std::complex<double> result = sqrt(xr * s) * pi_bar / mRes;
-  result *= sqrt(xr * 8. * mPro * mRes / kinematics->initial->momentum(s));
-  result *= A_lam * a;
+    // Photo-coupling overall size of |A_1/2|^2 + |A_3/2|^2 is restriced from VMD
+    std::complex<double> A_lam = emGamma * M_PI * mRes * double(J + 1) / (2. * mPro * pi_bar * pi_bar);
+    A_lam = sqrt(xr * A_lam);
 
-  // FACTPR PF 4 PI SOMETIMES FACTORED OUT
-  result *= sqrt(4. * M_PI * M_ALPHA);
+    std::complex<double> result = sqrt(xr * s) * pi_bar / mRes;
+    result *= sqrt(xr * 8. * mPro * mRes / kinematics->initial->momentum(s));
+    result *= A_lam * a;
 
-  return result;
+    // FACTPR PF 4 PI SOMETIMES FACTORED OUT
+    result *= sqrt(4. * M_PI * M_ALPHA);
+
+    return result;
 };
 
 // Hadronic decay helicity amplitude for the R -> J/psi p process
 std::complex<double> jpacPhoto::baryon_resonance::hadronic_coupling(int lam_f)
 {
-  // Hadronic coupling constant g, given in terms of branching ratio xBR
-  std::complex<double> g;
-  g  = 8. * M_PI * xBR * gamRes;
-  g *=  mRes * mRes * double(J + 1) / 6.;
-  g /= pow(pf_bar, double(2 * l_min + 1));
-  g = sqrt(xr * g);
+    // Hadronic coupling constant g, given in terms of branching ratio xBR
+    std::complex<double> g;
+    g  = 8. * M_PI * xBR * gamRes;
+    g *=  mRes * mRes * double(J + 1) / 6.;
+    g /= pow(pf_bar, double(2 * l_min + 1));
+    g = sqrt(xr * g);
 
-  std::complex<double> gpsi;
-  gpsi = g * pow(kinematics->final->momentum(s), l_min);
+    std::complex<double> gpsi;
+    gpsi = g * pow(kinematics->final->momentum(s), l_min);
 
-  return gpsi;
+    return gpsi;
 };
