de568524e8cf72651a3e13582da7cfee4da75af8
Daniel Winney <dwinney@iu.edu>
2020-12-03 23:24:18 -0500

    everything consistent with new kinematics constructor

--- include/reaction_kinematics.hpp
+++ include/reaction_kinematics.hpp
@@ -41,9 +41,6 @@ namespace jpacPhoto
         // Empty constructor,
         // defaults to compton scattering: gamma p -> gamma p
         reaction_kinematics()
-        : mX(0.), mX2(0.), 
-          mBar(mPro), mBar2(mPro2),
-          Q2(0.)
         {
             initial   = new two_body_state(0., mPro2);
             eps_gamma = new polarization_vector(initial);
@@ -58,9 +55,7 @@ namespace jpacPhoto
         // defaults to proton as baryon and real photon
         // string ID is deprecated but kept for backward compatibility
         reaction_kinematics(double _mX, std::string id = "")
-        : mX(_mX), mX2(_mX*_mX),
-          mBar(mPro), mBar2(mPro2),
-          Q2(0.)
+        : mX(_mX), mX2(_mX*_mX)
         {
             initial   = new two_body_state(0., mPro2);
             eps_gamma = new polarization_vector(initial);
@@ -72,34 +67,34 @@ namespace jpacPhoto
         };
 
 
-        // Constructor with a set mX and baryon mass mB
+        // Constructor with a set mX and baryon mass mR
         // defaults to real photon
-        reaction_kinematics(double _mX, double mB)
+        reaction_kinematics(double _mX, double _mR)
         : mX(_mX), mX2(_mX*_mX),
-          mBar(mB), mBar2(mB*mB),
-          Q2(0.)
+          mR(_mR), mR2(_mR*_mR)
         {
-            initial   = new two_body_state(0., mB*mB);
+            initial   = new two_body_state(0., mPro2);
             eps_gamma = new polarization_vector(initial);
             target    = new dirac_spinor(initial);
 
-            final     = new two_body_state(_mX*_mX, mB*mB);
+            final     = new two_body_state(_mX*_mX, _mR*_mR);
             eps_vec   = new polarization_vector(final);
             recoil    = new dirac_spinor(final);
         };
 
-        // Constructor with a set mV and baryon mass mB
-        // and q2 > 0 for a virtual photon
-        reaction_kinematics(double _mX, double mB, double q2)
+        // Constructor with a set mV and baryon mass mR
+        // and massive beam and target
+        reaction_kinematics(double _mX, double _mR, double _mT, double _mB = 0.)
         : mX(_mX), mX2(_mX*_mX),
-          mBar(mB), mBar2(mB*mB),
-          Q2(q2)
+          mR(_mR), mR2(_mR*_mR),
+          mB(_mB), mB2(_mB*_mB),
+          mT(_mT), mT2(_mT*_mT)
         {
-            initial   = new two_body_state(-q2, mB*mB);
+            initial   = new two_body_state(_mB*_mB, _mT*_mT);
             eps_gamma = new polarization_vector(initial);
             target    = new dirac_spinor(initial);
 
-            final     = new two_body_state(_mX*_mX, mB*mB);
+            final     = new two_body_state(_mX*_mX, _mR*_mR);
             eps_vec   = new polarization_vector(final);
             recoil    = new dirac_spinor(final);
         };
@@ -114,11 +109,14 @@ namespace jpacPhoto
 
         // ---------------------------------------------------------------------------
         // Masses
+        
+        double mB = 0., mB2 = 0.;       // mass and mass squared of the "beam" 
+        double mX = 0., mX2 = 0.;       // mass and mass squared of the produced particle
 
-        double mX = 0., mX2 = 0.;     // mass and mass squared of the produced particle
-        double mBar = 0., mBar2 = 0.; // mass nd mass squared of the baryon 
-        double Q2 = 0.; // virtuality of the photon
-        inline double Wth(){ return (mX + mBar); }; // square root of the threshold
+        double mT = mPro, mT2 = mPro2;  // mass of the target, assumed to be proton unless overriden
+        double mR = mPro, mR2 = mPro2;  // mass of the recoil baryon, assumed to be proton unless overriden
+
+        inline double Wth(){ return (mX + mR); }; // square root of the threshold
         inline double sth(){ return Wth() * Wth(); }; // final state threshold
 
         // Change the meson mass
@@ -140,23 +138,12 @@ namespace jpacPhoto
             final->set_mV2(m2);
         };
 
-        // Change the baryon mass squared
-        inline void set_mB2(double m2)
-        {
-            mBar  = sqrt(m2);
-            mBar2 = m2;
-
-            // also update the meson mass in two_body_state
-            initial->set_mB2(m2);
-            final->set_mB2(m2);
-        };
-
         // Change virtuality of the photon
         // Q2 > 0
         inline void set_Q2(double q2)
         {
             if (q2 > 0) { std::cout << "Caution! set_Q2(x) requires x > 0! \n"; }
-            Q2 = q2;
+            mB2 = -q2;
             initial->set_mV2(-q2);
         };
 
@@ -186,7 +173,7 @@ namespace jpacPhoto
             std::complex<double> qdotqp = initial->momentum(s) * final->momentum(s);
             std::complex<double> E1E3   = initial->energy_V(s) * final->energy_V(s);
 
-            double result = t - mX2 + Q2 + 2.*abs(E1E3);
+            double result = t - mX2 - mB2 + 2.*abs(E1E3);
             result /= 2. * abs(qdotqp);
 
             return result;
@@ -203,25 +190,25 @@ namespace jpacPhoto
         inline double t_man(double s, double theta)
         {
             std::complex<double> qdotqp = initial->momentum(s) * final->momentum(s);
-            std::complex<double> E1E3 = initial->energy_V(s) * final->energy_V(s);
+            std::complex<double> E1E3   = initial->energy_V(s) * final->energy_V(s);
 
-            return mX2 - Q2 - 2. * abs(E1E3) + 2. * abs(qdotqp) * cos(theta);
+            return mX2 + mB2 - 2. * abs(E1E3) + 2. * abs(qdotqp) * cos(theta);
         };
 
         inline double u_man(double s, double theta)
         { 
-            return mX2 - Q2 + 2. * mBar2 - s - t_man(s, theta);
+            return mX2 + mB2 + mT2 + mR2 - s - t_man(s, theta);
         };
 
         // Scattering angles in t and u channel frames
         inline std::complex<double> z_t(double s, double theta)
         {
             double t = t_man(s, theta);
-            std::complex<double> p_t = sqrt(xr * t - 4. * mBar2) / 2.;
-            std::complex<double> q_t = sqrt(xr * Kallen(t, mX2, -Q2)) / sqrt(xr * 4. * t);
+            std::complex<double> p_t = sqrt(xr * Kallen(t, mT2, mR2)) / sqrt(xr * 4. * t);
+            std::complex<double> q_t = sqrt(xr * Kallen(t, mX2, mB2)) / sqrt(xr * 4. * t);
 
             std::complex<double> result;
-            result = 2. * s + t - 2. * mBar2 - mX2 + Q2; // s - u
+            result = 2. * s + t - mT2 - mR2 - mX2 - mB2; // s - u
             result /= 4. * p_t * q_t;
 
             return result;
