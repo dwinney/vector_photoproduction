002e847909b272f14ce29c2b03ee86ce883a3116
Daniel Winney <dwinney@iu.edu>
2020-12-04 15:20:41 -0500

    renamed average_asymmetry

--- executables/pentaquark/average_asymmetry.cpp
+++ executables/pentaquark/average_asymmetry.cpp
@@ -69,34 +69,31 @@ int main( int argc, char** argv )
     auto Pc4457_3m = new baryon_resonance(ptr, 3, -1, 4.4573, 6.4E-3, "P_{c}(4457)");
     Pc4457_3m->set_params({0.01, .7071});
 
-    auto Pc4457_5m = new baryon_resonance(ptr, 5, -1, 4.4573, 6.4E-3, "P_{c}(4457)");
+    auto Pc4457_5m = new baryon_resonance(ptr, 5, 1, 4.4573, 6.4E-3, "P_{c}(4457)");
     Pc4457_5m->set_params({0.01, .7071});
 
 
     // Add to the sum
-    auto sumA = new amplitude_sum (ptr, {background, Pc4312_1m, Pc4440_3m}, "A");
-    auto sumB = new amplitude_sum (ptr, {background, Pc4312_3m, Pc4440_1m}, "B");
-    auto sumC = new amplitude_sum (ptr, {background, Pc4312_3m, Pc4440_3p}, "C");
+    auto sumA = new amplitude_sum (ptr, {background, Pc4312_1m, Pc4440_3m, Pc4457_1m}, "A");
+    auto sumB = new amplitude_sum (ptr, {background, Pc4312_3m, Pc4440_1m, Pc4457_3m}, "B");
+    auto sumC = new amplitude_sum (ptr, {background, Pc4312_3m, Pc4440_3p, Pc4457_5m}, "C");
 
     // ---------------------------------------------------------------------------
     // Choose which scenario to plot
     std::vector<amplitude*> amps;
     amps.push_back(background);
-    // amps.push_back(sumA);
-    // amps.push_back(sumB);
-    // amps.push_back(sumC);
+    amps.push_back(sumA);
+    amps.push_back(sumB);
+    amps.push_back(sumC);
 
     int N = 100;
     bool PRINT_TO_COMMANDLINE = true;
 
-    double ymax =  1.;
-    double ymin = -0.1;
+    double ymax =  0.2;
+    double ymin = -0.05;
 
     double Emin = E_beam(ptr->Wth()) + EPS;
     double Emax = 12.;
-
-    // double Emin = 10.;
-    // double Emax = 10.1;
 
     std::string filename = "sigma_integrated.pdf";
 
@@ -109,42 +106,47 @@ int main( int argc, char** argv )
 
     // ---------------------------------------------------------------------------
     // scan over theta
-    // for (int n = 0; n < amps.size(); n++)
-    // {
-    //     std::cout << "\nPrinting amplitude " << amps[n]->identifier << ".\n";
+    for (int n = 0; n < amps.size(); n++)
+    {
+        std::cout << "\nPrinting amplitude " << amps[n]->identifier << ".\n";
 
-    //     auto F = [&](double theta)
-    //     {
+        auto F = [&](double theta)
+        {
 
-    //         auto f = [&](double E)
-    //         {
-    //             double W = W_cm(E);
-    //             double t = ptr->t_man(W*W, theta * deg2rad);
-    //             return amps[n]->beam_asymmetry_4pi(W*W, t);
-    //         };
+            auto f = [&](double E)
+            {
+                double W = W_cm(E);
+                double t = ptr->t_man(W*W, theta * deg2rad);
+                return amps[n]->beam_asymmetry_4pi(W*W, t);
+            };
 
-    //         ROOT::Math::GSLIntegrator ig(ROOT::Math::IntegrationOneDim::kADAPTIVE, ROOT::Math::Integration::kGAUSS61);
-    //         ROOT::Math::Functor1D wF(f);
-    //         ig.SetFunction(wF);
+            ROOT::Math::GSLIntegrator ig(ROOT::Math::IntegrationOneDim::kADAPTIVE, ROOT::Math::Integration::kGAUSS61);
+            ROOT::Math::Functor1D wF(f);
+            ig.SetFunction(wF);
             
-    //         return ig.Integral(Emin, Emax);
-    //     };
+            return ig.Integral(Emin, Emax) / (Emax - Emin);
+        };
 
-    //     std::array<std::vector<double>, 2> x_fx = vec_fill(N, F, 0., 90., PRINT_TO_COMMANDLINE);
-    //     plotter->AddEntry(x_fx[0], x_fx[1], amps[n]->identifier);
-    // }
+        std::array<std::vector<double>, 2> x_fx = vec_fill(N, F, 0., 90., PRINT_TO_COMMANDLINE);
+        plotter->AddEntry(x_fx[0], x_fx[1], amps[n]->identifier);
+    }
 
-    // // Add a header to legend to specify the fixed energy
-    // plotter->SetLegend(0.2, 0.7);
+    // Add a header to legend to specify the fixed energy
+    plotter->SetLegend(0.2, 0.7);
 
-    // plotter->SetXaxis("#theta", 0., 90.);
+    plotter->SetXaxis("#theta", 0., 90.);
 
-    // // To change the range of the Y-axis or the position of the Legend change the arguments here
-    // plotter->SetYaxis("#Sigma integrated over E_{#gamma} = \{th, 12.\}", ymin, ymax);
+    // To change the range of the Y-axis or the position of the Legend change the arguments here
+    plotter->SetYaxis("#Sigma averaged over E_{#gamma}", ymin, ymax);
 
+    std::string file1 = "theta_" + filename;
+    plotter->Plot(file1.c_str());
 
-    // plotter->Plot(filename.c_str());
-
+    // clear
+    plotter->ClearData();
+    
+    // ---------------------------------------------------------------------------
+    // scan over energy
     for (int n = 0; n < amps.size(); n++)
     {
         std::cout << "\nPrinting amplitude " << amps[n]->identifier << ".\n";
@@ -163,7 +165,7 @@ int main( int argc, char** argv )
             
             double t_min = amps[n]->kinematics->t_man(W*W, 0.);
             double t_max = amps[n]->kinematics->t_man(W*W, M_PI);
-            return ig.Integral(t_max, t_min);
+            return ig.Integral(t_max, t_min) / (t_min - t_max);
         };
 
         std::array<std::vector<double>, 2> x_fx = vec_fill(N, F, Emin, Emax, PRINT_TO_COMMANDLINE);
@@ -176,10 +178,10 @@ int main( int argc, char** argv )
     plotter->SetXaxis("E_{#gamma}", Emin, Emax);
 
     // To change the range of the Y-axis or the position of the Legend change the arguments here
-    plotter->SetYaxis("#Sigma integrated over t", ymin, ymax);
+    plotter->SetYaxis("#Sigma averaged over t", ymin, ymax);
 
-
-    plotter->Plot(filename.c_str());
+    std::string file2 = "egam_" + filename;
+    plotter->Plot(file2.c_str());
 
     return 1.;
 };
