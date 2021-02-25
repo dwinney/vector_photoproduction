8fdcbea7fcb8a92c883e27321a5872f85549c4dc
Daniel Winney <dwinney@iu.edu>
2021-02-25 13:46:11 -0500

    discontinuity is double and uses GSL

--- include/one_loop/box_discontinuity.hpp
+++ include/one_loop/box_discontinuity.hpp
@@ -13,71 +13,64 @@
 #include "amplitudes/amplitude.hpp"
 #include "amplitudes/reaction_kinematics.hpp"
 
-#include "cubature.h"
+#include "Math/IntegratorMultiDim.h"
 
 namespace jpacPhoto
 {
-    // Wrapper class for integration with cubature
-    struct disc_integrand
-    {
-        disc_integrand(amplitude * left, amplitude * right)
-        : _initialAmp(left), _finalAmp(right)
-        {};
-
-        // External helicities and center-of-mass energy / angle
-        inline void set_externals(std::array<int,4> helicities, double s, double theta)
-        {
-            _lam_gam = helicities[0];
-            _lam_tar = helicities[1];
-            _lam_vec = helicities[2];
-            _lam_rec = helicities[3];
-
-            _s = s; _theta = theta;
-        };
-        double _s, _theta;
-        int _lam_gam, _lam_tar, _lam_vec, _lam_rec;
-
-        // Evaluate the product of subamplitudes
-        std::complex<double> eval(double theta_gam, double phi_gam);
-        
-        // Save the pointers to the two sub-amplitudes
-        amplitude * _initialAmp;
-        amplitude * _finalAmp;
-    };
-
-    // Actual discontinuity
     class box_discontinuity
     {
         public: 
         box_discontinuity(amplitude * left, amplitude * right)
+        : _initialAmp(left), _finalAmp(right)
         {
-            _integrand = new disc_integrand(left, right);
-        };
+            // Make sure the left and right amplitudes match!
+            // Check the spins of the intermediate state
+            _jp_left  = left->_kinematics->_jp;
+            _jp_right = right->_kinematics->_jp;
 
-        ~box_discontinuity()
-        {
-            delete _integrand;
+            if (_jp_left != _jp_right) 
+            {
+                std::cout << std::left;
+                std::cout << std::setw(40) << "box_amplitude: Intermediate state between sub-amplitudes dont match!" << std::endl;
+                std::cout << std::setw(20) << _initialAmp->_identifier << ": \t (" << _jp_left[0]  << ", " << _jp_left[1]  << ")\n";
+                std::cout << std::setw(20) << _finalAmp->_identifier   << ": \t (" << _jp_right[0] << ", " << _jp_right[1] << ")\n";
+                std::cout << "Returning 0!" << std::endl;
+
+                _matchError = true;
+            };
+
+            // But also masses
+            if ((std::abs(left->_kinematics->_mX - right->_kinematics->_mX) > 1.E-4) 
+             || (std::abs(left->_kinematics->_mR - right->_kinematics->_mR) > 1.E-4))
+            {
+                _matchError = true;
+            };
+            
+            // IF they match, get the spin and therefor helicities of the intermediate meson
+            _intermediate_helicities = get_helicities(_jp_left[0]);
         };
 
         // Evaluate the discontinuity integrated over intermediate phase space
-        std::complex<double> eval(double s);
+        double eval(double s);
 
-        inline void set_externals(std::array<int,4> helicities, double s, double theta)
+        // Pass values that come from the external gamma p -> V p reaction
+        inline void set_externals(std::array<int,4> helicities, double theta)
         {
-            _external_s = s;
             _external_theta = theta; 
             _external_helicities = helicities;
         };
 
-        inline double s(){return _external_s;};
-
         private:
-        double _external_s, _external_theta;
+        double _external_theta;
         std::array<int,4> _external_helicities;
 
-        // Wrapper class for integration with cubature
-        disc_integrand * _integrand;
-        static int wrapped_integrand(unsigned ndim, const double *in, void *fdata, unsigned fdim, double *fval);
+        // Individual tree amplitudes
+        amplitude * _initialAmp;
+        amplitude * _finalAmp;
+
+        std::array<int,2> _jp_left, _jp_right;
+        std::vector< std::array<int,4> > _intermediate_helicities;
+        bool _matchError = false;
     };
 };
 
--- src/one_loop/box_discontinuity.cpp
+++ src/one_loop/box_discontinuity.cpp
@@ -9,87 +9,60 @@
 #include "one_loop/box_discontinuity.hpp"
 
 // ---------------------------------------------------------------------------
-// Evaluate the integrand of the discontinuity as the product of the two
-// sub amplitudes in terms of their respective t's
-std::complex<double> jpacPhoto::disc_integrand::eval(double theta_gam, double phi_gam)
-{
-    // Check that the two amplitudes line up
-    std::array<int,2> _jp_left  = _initialAmp->_kinematics->_jp;
-    std::array<int,2> _jp_right = _finalAmp->_kinematics->_jp;
-
-    if (_jp_left != _jp_right) 
-    {
-        std::cout << std::left;
-        std::cout << std::setw(40) << "box_amplitude: Intermediate state between sub-amplitudes dont match!" << std::endl;
-        std::cout << std::setw(20) << _initialAmp->_identifier << ": \t (" << _jp_left[0]  << ", " << _jp_left[1]  << ")\n";
-        std::cout << std::setw(20) << _finalAmp->_identifier   << ": \t (" << _jp_right[0] << ", " << _jp_right[1] << ")\n";
-        std::cout << "Returning 0!" << std::endl;
-
-        return 0.;
-    };
-
-    std::vector< std::array<int,4> > intermediate_helicities = get_helicities(_jp_left[0]);
-
-    // calculate the sub-process momentum transfers
-    double t_gam        = _initialAmp->_kinematics->t_man(_s, theta_gam);
-
-    double costheta_vec = cos(_theta) * cos(theta_gam) + sin(_theta) * sin(theta_gam) * cos(phi_gam);
-    double theta_vec    = TMath::ACos(costheta_vec);
-    double t_vec        = _finalAmp->_kinematics->t_man(_s, theta_gam);
-
-    // 
-    std::complex<double> phase_space;
-    phase_space  = _initialAmp->_kinematics->_final_state->momentum(_s);
-    phase_space *= 2. / sqrt(_s);
-
-    // Sum over intermediate helicities 
-    std::complex<double> result = 0.;
-    for (int i = 0; i < 4*_jp_left[0]+2; i++)
-    {
-        int lam_meson  = intermediate_helicities[i][2];
-        int lam_baryon = intermediate_helicities[i][3];
-
-        std::complex<double> temp;
-        temp  = _initialAmp->helicity_amplitude({_lam_gam, _lam_tar, lam_meson, lam_baryon}, _s, t_gam);
-        temp *= _finalAmp->helicity_amplitude(  {_lam_vec, _lam_rec, lam_meson, lam_baryon}, _s, t_vec);
-        
-        result += temp;
-    };
-    
-    return result * phase_space / (64. * PI*PI);
-};
-
-// ---------------------------------------------------------------------------
 // Evaluate the product of sub-amplitudes integrating over intermediate phase-space
-std::complex<double> jpacPhoto::box_discontinuity::eval(double s)
+double jpacPhoto::box_discontinuity::eval(double s)
 {
-    // Pass stuff to the integrand
-    _integrand->set_externals(_external_helicities, s, _external_theta);
+    // Check the intermediate state matched
+    if (_matchError) return 0.;
+
+    // make sure the external helicites get passed correctly
+    int lam_gam = _external_helicities[0];
+    int lam_tar = _external_helicities[1];
+    int lam_vec = _external_helicities[2];
+    int lam_rec = _external_helicities[3];
+
+    auto dF = [&](const double * x)
+    {
+        double theta_gam = x[0];
+        double phi_gam   = x[1];
+            
+        // calculate the sub-process momentum transfers
+        double t_gam        = _initialAmp->_kinematics->t_man(s, theta_gam);
+
+        double costheta_vec = cos(_external_theta) * cos(theta_gam) + sin(_external_theta) * sin(theta_gam) * cos(phi_gam);
+        double theta_vec    = TMath::ACos(costheta_vec);
+        double t_vec        = _finalAmp->_kinematics->t_man(s, theta_vec);
+
+        // Sum over intermediate helicities 
+        std::complex<double> result = 0.;
+        for (int i = 0; i < 4*_jp_left[0]+2; i++)
+        {
+            int lam_meson  = _intermediate_helicities[i][2];
+            int lam_baryon = _intermediate_helicities[i][3];
+
+            std::complex<double> left, right;
+            left  = _initialAmp->helicity_amplitude({lam_gam, lam_tar, lam_meson, lam_baryon}, s, t_gam);
+            right = _finalAmp->helicity_amplitude(  {lam_vec, lam_rec, lam_meson, lam_baryon}, s, t_vec);
+            result += left * right;
+        };
+
+        return real(result);
+    };
 
     // Integrate over theta_gamma = [0, pi] and phi = [0, 2pi]
-    double val[2], err[2];
     double min[2] = {0., 0.};
-    double max[2] = {PI, 2. * PI};
-    pcubature(2, wrapped_integrand, _integrand, 2, min, max, 2E5, 0, 1e-3, ERROR_INDIVIDUAL, val, err);
+    double max[2] = {PI, 2.*PI};
     
-    std::complex<double> result = val[0] + XI * val[1];
-    // debug(result);
-    return result;
+    ROOT::Math::IntegratorMultiDim ig(ROOT::Math::IntegrationMultiDim::kDEFAULT);
+    ROOT::Math::Functor wF(dF, 2);
+    ig.SetFunction(wF, 2);
+
+    double result = ig.Integral(min, max);
+
+    // Muliply by the two-body phase space
+    double phase_space;
+    phase_space  = real(_initialAmp->_kinematics->_final_state->momentum(s));
+    phase_space *= 2. / sqrt(s);
+
+    return result * phase_space / (64. * PI*PI);
 };
-
-// Static wrapper to pass all the necessary data to the integrater
-int jpacPhoto::box_discontinuity::wrapped_integrand(unsigned ndim, const double *in, void *fdata, unsigned fdim, double *fval)
-{
-    double theta_gam = in[0], phi_gam = in[1];
-    disc_integrand * integrand = (disc_integrand *) fdata;
-
-    std::complex<double> result;
-    result  = integrand->eval(theta_gam, phi_gam);
-    result *= sin(theta_gam); // Jacobian
-
-    fval[0] = std::real(result);
-    fval[1] = std::imag(result);
-
-    return 0;
-};
-
